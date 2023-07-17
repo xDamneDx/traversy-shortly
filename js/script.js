80911e7be8a1935cf84b44c597b8bcdeb37fdbb0
xDamneDx <xdamnedx@gmail.com>
2023-07-17 12:27:54 +0200

    add js link validation

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+function isValidURL(str) {
+  return /^(https?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(
+    str
+  );
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === "") {
+    errMsg.innerHTML = "Please enter something...";
+    input.classList.add("border-red");
+  } else if (!isValidURL(input.value)) {
+    errMsg.innerHTML = "Please enter a valid URL";
+    input.classList.add("border-red");
+  } else {
+    errMsg.innerHTML = "";
+    input.classList.remove("border-red");
+  }
+}
