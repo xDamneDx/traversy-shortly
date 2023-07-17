b29ba422e858a3d8a98b368576f804dbf453ad27
xDamneDx <xdamnedx@gmail.com>
2023-07-17 15:06:22 +0200

    add mobile menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
 
+btn.addEventListener("click", navToggle);
 linkForm.addEventListener("submit", formSubmit);
 
+// Toggle Mobile Menu:
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
+// Validate URL:
 function isValidURL(str) {
   return /^(https?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/g.test(
     str
